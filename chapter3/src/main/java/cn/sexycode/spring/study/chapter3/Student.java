58edf98b0df68f0db74d0869a1070ae00fab8988
qzz <qzzsunly@163.com>
2019-04-16 22:59:01 +0800

    自定义命名空间

---
+++ chapter3/src/main/java/cn/sexycode/spring/study/chapter3/Student.java
@@ -0,0 +1,32 @@
+package cn.sexycode.spring.study.chapter3;
+
+/**
+ *
+ */
+public class Student {
+    private String name;
+    private String age;
+
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
