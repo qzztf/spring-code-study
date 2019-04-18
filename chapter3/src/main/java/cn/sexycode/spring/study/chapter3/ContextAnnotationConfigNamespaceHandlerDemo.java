6e4912e8b0b7d4aebff35e5369aa694407fb674a
qzz <qinzaizhen@ddjf.com.cn>
2019-04-18 18:03:37 +0800

    自定义命名空间解析

---
+++ chapter3/src/main/java/cn/sexycode/spring/study/chapter3/ContextAnnotationConfigNamespaceHandlerDemo.java
@@ -0,0 +1,16 @@
+package cn.sexycode.spring.study.chapter3;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * 测试构造函数标签
+ * @author qzz
+ */
+public class ContextAnnotationConfigNamespaceHandlerDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions("ContextAnnotationConfigNamespaceHandlerDemo.xml");
+    }
+}
