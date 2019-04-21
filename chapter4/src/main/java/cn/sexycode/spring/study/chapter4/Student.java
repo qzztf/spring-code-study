806119775af5870bb5ca23d8076ec391d35ca7a1
qzz <qzzsunly@163.com>
2019-04-21 20:55:03 +0800

    bean实例化

---
+++ chapter4/src/main/java/cn/sexycode/spring/study/chapter4/Student.java
@@ -0,0 +1,26 @@
+package cn.sexycode.spring.study.chapter4;
+
+/**
+ *
+ */
+public class Student {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
