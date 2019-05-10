b35b4bf246944d43a9a07ebfa426d3d8d646ada6
qzz <qinzaizhen@ddjf.com.cn>
2019-05-10 17:23:50 +0800

    自定义命名空间解析

---
+++ chapter3/src/main/java/cn/sexycode/spring/study/chapter3/SimpleAdvice.java
@@ -0,0 +1,13 @@
+package cn.sexycode.spring.study.chapter3;
+
+import org.springframework.aop.BeforeAdvice;
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qinzaizhen
+ */
+@Component
+public class SimpleAdvice implements BeforeAdvice {
+
+}
