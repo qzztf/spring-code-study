efe340193229e1a7e652123e48d224a6d3a8034b
qzz <qzzsunly@163.com>
2019-09-04 12:22:13 +0800

    Formatter

--- chapter4/BeanWrapper.md
+++ chapter4/BeanWrapper.md
@@ -652,7 +652,7 @@ conversionService.addFormatter(new DateFormatter("yyyy-MM-dd"));
        private Date createTime;
    ```
 
-   指定格式为`yyyy-MM-dd`，这里我们需要`BeanWrapper`来触发格式化的动作。
+   指定格式为`yyyy-MM-dd`，这里我们借助`BeanWrapper`来触发格式化的动作。
 
    ```java
    DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
@@ -665,8 +665,14 @@ conversionService.addFormatter(new DateFormatter("yyyy-MM-dd"));
    
    //-----------
    // Tue Sep 03 00:00:00 CST 2019
+   
+// 将时间格式化 字符串
+   System.out.println("注解格式化日期：" + conversionService.convert(question.getCreateTime(), new TypeDescriptor(question.getClass().getDeclaredField("createTime")), TypeDescriptor.valueOf(String.class)));
+   
+   //---------
+   //注解格式化日期：2019-09-03
    ```
-
-   通过打印的信息，可以看到已经成功将字符串`parse`为`Date`。
+   
+   通过打印的信息，可以看到已经成功将字符串`parse`为`Date`，并将日期`format`为字符串。由于注解在字段上，我们只提供了`Date`的值，所以还需要通过`TypeDescriptor`将字段的附加信息传递进去，这样才能正确识别到字段上的注解。
 
 # `DirectFieldAccessor`
--- chapter4/src/main/java/cn/sexycode/spring/study/chapter4/FormatterConversionServiceDemo.java
+++ chapter4/src/main/java/cn/sexycode/spring/study/chapter4/FormatterConversionServiceDemo.java
@@ -2,6 +2,7 @@ package cn.sexycode.spring.study.chapter4;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 
@@ -11,7 +12,7 @@ import java.util.Date;
  * @author qzz
  */
 public class FormatterConversionServiceDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
         conversionService.addFormatter(new DateFormatter("yyyy-MM-dd"));
         Date date = new Date();
@@ -22,6 +23,8 @@ public class FormatterConversionServiceDemo {
         BeanWrapper beanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(question);
         beanWrapper.setConversionService(conversionService);
         beanWrapper.setPropertyValue("createTime", "2019-09-03");
+        //再试试将Date 格式化成 字符串
         System.out.println(question.getCreateTime());
+        System.out.println("注解格式化日期：" + conversionService.convert(question.getCreateTime(), new TypeDescriptor(question.getClass().getDeclaredField("createTime")), TypeDescriptor.valueOf(String.class)));
     }
 }
