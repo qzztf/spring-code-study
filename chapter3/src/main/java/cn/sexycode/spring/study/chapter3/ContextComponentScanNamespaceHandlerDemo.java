6f118c0093d3854e471aa88c939427b656c3a1ae
qzz <qzzsunly@163.com>
2019-12-24 23:19:07 +0800

    Spring 配置Bean的方式

--- chapter3/src/main/java/cn/sexycode/spring/study/chapter3/ContextComponentScanNamespaceHandlerDemo.java
+++ chapter3/src/main/java/cn/sexycode/spring/study/chapter3/ContextComponentScanNamespaceHandlerDemo.java
@@ -3,6 +3,8 @@ package cn.sexycode.spring.study.chapter3;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
+import java.util.Arrays;
+
 /**
  * 测试构造函数标签
  * @author qzz
@@ -12,5 +14,7 @@ public class ContextComponentScanNamespaceHandlerDemo {
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
         reader.loadBeanDefinitions("ContextComponentScanNamespaceHandlerDemo.xml");
+        System.out.println(Arrays.toString(factory.getBeanDefinitionNames()));
+        System.out.println(((SimpleAdvice) factory.getBean("simpleAdvice")).getCom());
     }
 }
--- chapter3/src/main/java/cn/sexycode/spring/study/chapter3/SimpleAdvice.java
+++ chapter3/src/main/java/cn/sexycode/spring/study/chapter3/SimpleAdvice.java
@@ -2,6 +2,7 @@ package cn.sexycode.spring.study.chapter3;
 
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,5 +10,21 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SimpleAdvice implements BeforeAdvice {
+
+    @Autowired
+    private Com com;
+
+    public Com getCom() {
+        return com;
+    }
+
+    public void setCom(Com com) {
+        this.com = com;
+    }
+
+    @Component
+    public static class Com{
+
+    }
 
 }
