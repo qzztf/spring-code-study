ac60054d61eab0979ae24f4fdb6422a92bd48760
qzz <qinzaizhen@ddjf.com.cn>
2020-04-21 18:17:45 +0800

    aop用法

--- chapter5/src/main/java/cn/sexycode/spring/study/chapter5/AopDemoApplication.java
+++ chapter5/src/main/java/cn/sexycode/spring/study/chapter5/AopDemoApplication.java
@@ -2,12 +2,17 @@ package cn.sexycode.spring.study.chapter5;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AopDemoApplication {
     public static void main(String[] args) {
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        /*DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
         xmlBeanDefinitionReader.loadBeanDefinitions("AopDemo.xml");
-        beanFactory.getBean(BusinessService.class).sayHello();
+        beanFactory.getBean(BusinessService.class).sayHello();*/
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("AopDemo.xml");
+        applicationContext.getBean(IBusinessService.class).sayHello();
     }
 }
